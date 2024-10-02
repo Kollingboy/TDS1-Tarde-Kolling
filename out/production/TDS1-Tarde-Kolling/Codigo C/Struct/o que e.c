4fe50d21ee134686713413c4f675908ec7252646
Kollingboy <joao.victhor.kolling@gmail.com>
2024-10-02 15:50:03 -0300

    atualizado

--- out/production/TDS1-Tarde-Kolling/Codigo C/Struct/o que e.c
+++
@@ -1,6 +0,0 @@
-/*O Que é Struct.
-
-Structs, também conhecidas como Registros, definem tipos de dados que agrupam variáveis sob um mesmo tipo de dado.
-A ideia de usar uma struct é permitir que, ao armazenar os dados de uma mesma entidade,
-isto possa ser feito com uma única variável.*/
-
