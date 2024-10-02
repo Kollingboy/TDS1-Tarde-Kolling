a2fce25effaebd5cad6a516e9b85d310e40127b1
Kollingboy <joao.victhor.kolling@gmail.com>
2024-10-02 16:04:07 -0300

    atualizado

--- out/production/TDS1-Tarde-Kolling/JavaScript/atv 4 IMC/script.js
+++
@@ -1,7 +0,0 @@
-function calcular(){
-    var num1 = parseFloat(document.getElementById("num1").value)
-    var num2 = parseFloat(document.getElementById("num2").value)
-    var resultado = document.getElementById("resultado")
-
-    resultado.textContent = num1 / (num2 * num2).toFixed(2)
-}
